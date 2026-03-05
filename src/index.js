b7df6e4d094392ebbc2e99ee6928eb9944b78ec9
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2026-03-05 15:40:39 +0500

    Add template engine

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { fileURLToPath } from "node:url"
 import * as path from "node:path"
 import express from "express"
+import ejs from "ejs"
 
 const PORT = parseInt( process.env.PORT || 3_000 )
 const __filename = fileURLToPath( import.meta.url )
@@ -10,7 +11,12 @@ const __dirname = path.dirname( __filename )
 
 const app = express()
 
-app.get( "/", ( _, res ) => res.send( { message: "OK" } ) )
+app.engine( "html", ejs.renderFile )
+app.set( "view engine", "html" )
+
+app.set( "views", path.join( __dirname, "views" ) )
+
+app.get( "/", ( _, res ) => res.render( "index", { title: "Welcome" } ) )
 
 //
 
