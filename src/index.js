ec11a555767e7b433eb03dd2edea483a787aa17f
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2026-03-05 16:22:33 +0500

    Add static directory

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ const __dirname = path.dirname( __filename )
 
 const app = express()
 
+app.use( express.static( path.join( __dirname, "..", "public" ) ) )
+
 app.engine( "html", ejs.renderFile )
 app.set( "view engine", "html" )
 
