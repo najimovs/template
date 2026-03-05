7e520b65eb8fad2737fb9211a6fc426138ea2afd
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2026-03-05 15:17:42 +0500

    Add express

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import { fileURLToPath } from "node:url"
 import * as path from "node:path"
+import express from "express"
 
 const PORT = parseInt( process.env.PORT || 3_000 )
 const __filename = fileURLToPath( import.meta.url )
 const __dirname = path.dirname( __filename )
 
-console.log( __filename )
-console.log( __dirname )
-console.log( PORT )
+//
+
+const app = express()
+
+app.get( "/", ( _, res ) => res.send( { message: "OK" } ) )
+
+//
+
+app.listen( PORT, () => {
+
+	console.info( `Server ready at: ${ PORT }` )
+} )
