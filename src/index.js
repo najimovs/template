dc6dca420adcd4058fee806837bc3ea5c61141d9
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2026-03-07 16:59:49 +0500

    Add expression example

--- src/index.js
+++ src/index.js
@@ -7,6 +7,33 @@ const PORT = parseInt( process.env.PORT || 3_000 )
 const __filename = fileURLToPath( import.meta.url )
 const __dirname = path.dirname( __filename )
 
+const images = [
+	{
+		imgURL: "/images/pic01.jpg",
+	},
+	{
+		imgURL: "/images/pic02.jpg",
+	},
+	{
+		imgURL: "/images/pic03.jpg",
+	},
+	{
+		imgURL: "/images/pic04.jpg",
+	},
+	{
+		imgURL: "/images/pic05.jpg",
+	},
+	{
+		imgURL: "/images/pic06.jpg",
+	},
+	{
+		imgURL: "/images/pic07.jpg",
+	},
+	{
+		imgURL: "/images/pic08.jpg",
+	}
+]
+
 //
 
 const app = express()
@@ -20,7 +47,7 @@ app.set( "views", path.join( __dirname, "views" ) )
 
 app.get( "/", ( _, res ) => res.render( "index" ) )
 
-app.get( "/elements", ( _, res ) => res.render( "elements" ) )
+app.get( "/elements", ( _, res ) => res.render( "elements", { images } ) )
 app.get( "/left-sidebar", ( _, res ) => res.render( "left-sidebar" ) )
 app.get( "/right-sidebar", ( _, res ) => res.render( "right-sidebar" ) )
 app.get( "/no-sidebar", ( _, res ) => res.render( "no-sidebar" ) )
